e35df1cacb82b49da55ffb2a7a44bd2a9a29cbbb
TheCodingStache <cs4415161@teilar.gr>
2020-01-21 10:13:45 +0200

    Initial Commit for Dental Logs

---
+++ app/src/main/java/com/example/dentallogs/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dentallogs;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton logIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        TextView signIn = findViewById(R.id.signUpText);
+        logIn = findViewById(R.id.logIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(signUp);
+            }
+        });
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lab = new Intent(MainActivity.this, LabSelectionActivity.class);
+                startActivity(lab);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/dentallogs/SignUpActivity.java
@@ -0,0 +1,31 @@
+package com.example.dentallogs;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        TextView signUp = findViewById(R.id.signInText);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
